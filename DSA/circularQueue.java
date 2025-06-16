8cb3210472c292fac5b2430cd9122a1ada16ee87
Sidra Chaudhari <sidrasc05@gmail.com>
2025-06-16 11:06:03 +0530

    proper segregation

--- DSA/circularQueue.java
+++ DSA/circularQueue.java
@@ -11,7 +11,7 @@ public class circularQueue{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         CQ c= new CQ();
-
+        
         sc.close();
     }
 }
---
+++ DSA/tree.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+class TreeNode {
+    int data;
+    TreeNode left, right;
+
+    TreeNode(int value) {
+        data = value;
+        left = right = null;
+    }
+}
+
+public class tree {
+    TreeNode root;
+
+    // Insert node using recursion
+    public TreeNode insert(TreeNode node, int value) {
+        if (node == null) {
+            return new TreeNode(value);
+        }
+
+        if (value < node.data) {
+            node.left = insert(node.left, value);
+        } else if (value > node.data) {
+            node.right = insert(node.right, value);
+        }
+
+        return node;
+    }
+
+    // In-order Traversal (Left, Root, Right)
+    public void inOrder(TreeNode node) {
+        if (node != null) {
+            inOrder(node.left);
+            System.out.print(node.data + " ");
+            inOrder(node.right);
+        }
+    }
+
+    // Pre-order Traversal (Root, Left, Right)
+    public void preOrder(TreeNode node) {
+        if (node != null) {
+            System.out.print(node.data + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    // Post-order Traversal (Left, Right, Root)
+    public void postOrder(TreeNode node) {
+        if (node != null) {
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.print(node.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        tree tree = new tree();
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no. elements to put in the tree:");
+        int n=sc.nextInt();
+        System.out.println("Enter the elements to put in the tree:");
+        for(int i=0;i<n;i++){
+            int ele=sc.nextInt();
+            tree.root=tree.insert(tree.root,ele);
+        }
+        // Traversals
+        System.out.print("In-order: ");
+        tree.inOrder(tree.root);
+        System.out.println();
+
+        System.out.print("Pre-order: ");
+        tree.preOrder(tree.root);
+        System.out.println();
+
+        System.out.print("Post-order: ");
+        tree.postOrder(tree.root);
+        System.out.println();
+    }
+}
