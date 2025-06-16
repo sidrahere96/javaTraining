c00b84fc71361d32cf91f6ebb7e8f21f3eeff4e0
Sidra Chaudhari <sidrasc05@gmail.com>
2025-06-16 14:15:22 +0530

    insertion sort done

--- DSA/insertionSort.java
+++ DSA/insertionSort.java
@@ -1,18 +1,20 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class insertSort {
+public class insertionSort {
     public static void sort(int[] arr) {
-        int n = arr.length;
+        int n=arr.length;
 
-        for (int i = 1; i < n; i++) {
+        for(int i=1;i<n;i++) {
             int key = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
+            int j=i-1;
+
+            while(j>=0 && arr[j] > key) {
+                arr[j+1]=arr[j];
                 j--;
             }
-            arr[j + 1] = key;
+
+            arr[j+1]=key;
         }
     }
 
