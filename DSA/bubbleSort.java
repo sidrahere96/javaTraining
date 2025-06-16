5140ca44eeac00d818259eebf990768229235972
Sidra Chaudhari <sidrasc05@gmail.com>
2025-06-16 13:49:11 +0530

    bubble sort done

---
+++ DSA/bubbleSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class bubbleSort {
+    public static void sort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if(arr[j]>arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no. elements in array:");
+        int n=sc.nextInt();
+        int[] arr=new int [n];
+        System.out.println("Enter the elements to put in the array:");
+        for(int i=0;i<n;i++){
+            int ele=sc.nextInt();
+            arr[i]=ele;
+        }
+        System.out.println("ARRAY IS: "+Arrays.toString(arr));
+        sort(arr);
+        System.out.println("SORTED ARRAY IS: "+Arrays.toString(arr));
+    }
+}
